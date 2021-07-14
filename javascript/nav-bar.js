b77b77834f794aad1f291811f2345ec556af80e5
Chirag <chiragsimkhada@gmail.com>
2021-07-14 21:22:38 +0545

    Made overall website responsive

--- javascript/nav-bar.js
+++ javascript/nav-bar.js
@@ -2,3 +2,20 @@ window.addEventListener("scroll", function () {
   var header = this.document.querySelector("#head-top");
   header.classList.toggle("sticky", window.scrollY > 10);
 });
+
+window.onload = function () {
+  var i = 0;
+  text = "Your Pets!";
+  function type() {
+    if (i == 0) {
+      document.getElementById("moving-text").innerHTML = text.charAt(i);
+      i++;
+      setTimeout(type, 500);
+    } else if (i < text.length) {
+      document.getElementById("moving-text").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(type, 500);
+    }
+  }
+  type();
+};
